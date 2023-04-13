4d559e53a56edf3a02b723af92d55f958074ae5d
Shobhnik13 <shobhnikw@gmail.com>
2023-04-14 03:39:07 +0530

    started server

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,11 @@
 const express=require('express')
+const notes=require('./data/notes.js')
 const app=express()
+app.get('/',(req,res)=>{
+    res.send('Success')
+})
+app.get('/api/notes',(req,res)=>{
+res.json(notes)
+})
 // creating server 
 app.listen(5000,console.log('server started at port 5000'))
