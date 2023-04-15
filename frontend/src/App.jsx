16811d01d3a35b158aa45bca12e205ef48865b68
Shobhnik13 <shobhnikw@gmail.com>
2023-04-16 02:07:04 +0530

    added css navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div className="App">
+      <Header/>
+      <main style={{minHeight:'93vh'}}></main>
+      <Footer/>
     </div>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Navbar,Nav, NavDropdown, Form, FormControl, Container, Button } from 'react-bootstrap'
+const Header = () => {
+  return (
+    <div>
+     <Navbar bg="primary" expand="lg" variant='dark'>
+      <Container fluid>
+        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+            <Nav className='m-auto'>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+           
+          </Form>
+            </Nav>
+          <Nav
+            className="me-auto my-2 my-lg-0 mr-auto"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">My Notes</Nav.Link>
+            <NavDropdown title="shobhnik" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+           
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Header
