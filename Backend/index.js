09303232479b4ea4384bb19097a513efb8309d42
Shobhnik13 <shobhnikw@gmail.com>
2023-04-18 15:02:44 +0530

    fixed backend issue

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,17 @@
 const express=require('express')
 const dotenv=require('dotenv')
 const notes=require('./data/notes.js')
-
+const bodyParser=require('body-parser')
+const cors=require('cors')
 const app=express()
 dotenv.config();
+//middlewares
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors())
+
 
 app.get('/',(req,res)=>{
     res.send('Success')
