eb65001aa6921cb38c81051c18862daf6db37b8c
Shobhnik13 <shobhnikw@gmail.com>
2023-04-18 12:00:40 +0530

    created card component

---
+++ frontend/src/assets/Notes.js
@@ -0,0 +1,21 @@
+ const Notes=[
+    {
+        id:1,
+        title:'Started learning mern stack',
+        content:'This notes app was made using mern stack',
+        category:'Learning',
+    },
+    {
+        id:2,
+        title:'Start watching movie ',
+        content:'Need to watch movie at weekend',
+        category:'Entertainment',
+    },
+    {
+        id:3,
+        title:'Complete the assignments',
+        content:'Complete the assignments by the end of the week',
+        category:'College',
+    },
+]
+export default Notes;
--- frontend/src/screens/MyNotes/MyNotes.jsx
+++ frontend/src/screens/MyNotes/MyNotes.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import MainScreen from '../../components/MainScreen'
 import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
+import Notes from '../../assets/Notes'
 
 const MyNotes = () => {
+  const deleteNote=()=>{}
   return (
     <MainScreen title='Welcome back user'>
         <Link to='/createnote'>
         <Button style={{marginLeft:10,marginBottom:6}} size='lg'>
             Create a new note
         </Button>
+        {
+          Notes.map((item)=>{
+         return(
+         <Card style={{margin:10}} key={item.id}>
+            <Card.Header style={{display:"flex"}}>
+              <span
+              style={{
+                color:'black',
+                textDecoration:'none',
+                flex:'1',
+                cursor:'pointer',
+                alignSelf:'center',
+                fontSize:18,
+              }}>{item.title}</span>
+                 <div>
+                   <Button href={`/notes/${item.id}`}>Edit</Button>
+                    <Button variant='danger' className='ml-2 mr-2' onClick={()=>deleteNote(item.id)}>Delete</Button>
+                </div>
+              </Card.Header>
+          </Card>
+         )
+          })
+        }
         </Link>
     </MainScreen>
   )
