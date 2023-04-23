30b49d70701768e78c873a2889d70e5a32b8938d
Shobhnik13 <shobhnikw@gmail.com>
2023-04-24 03:21:32 +0530

    started adding redux

--- frontend/src/screens/LoginScreen/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen/LoginScreen.jsx
@@ -5,37 +5,19 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Loading from '../../components/Loading'
 import ErrorMessage from '../../components/ErrorMessage'
-
+import { useDispatch } from 'react-redux'
 const LoginScreen = () => {
 const [email,setEmail]=useState('')
 const [password,setPassword]=useState('')
-const [err,setErr]=useState('')
-const [loading,setLoading]=useState(false)
+const dispatch=useDispatch()
 const submitHandler=async(e)=>{
   e.preventDefault()
-  // to provide json data we need to create a config which contain headers 
-  try{
-    const config={
-      headers:{
-        "Content-type":"application/json"
-      }
-    }
-    // setLoading(true)
-    const {data}=await axios.post('http://localhost:5000/api/users/login',{email,password},config)
-    console.log(data)
-    localStorage.setItem('userInfo',JSON.stringify(data))
-    setLoading(false)
-  }
-  catch(error){
-      setErr(error.response.data.message)
-      setLoading(false)
-  }
 }
   return (
     <MainScreen title='LOGIN'>
         <div className='loginContainer'>
-          {err && <ErrorMessage variant='danger'>{err}</ErrorMessage>}
-          {loading && <Loading/>}
+          {/* {error && <ErrorMessage variant='danger'>{error}</ErrorMessage>} */}
+          {/* {loading && <Loading/>} */}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId='formBasicEmail'>
             <Form.Label>Email address</Form.Label>
