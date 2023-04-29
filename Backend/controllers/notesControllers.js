08dbb45ba992ec2849d2cbebca5f2c5be8cb3c91
Shobhnik13 <shobhnikw@gmail.com>
2023-04-29 17:16:15 +0530

    completed CRUD api functionallity

--- Backend/controllers/notesControllers.js
+++ Backend/controllers/notesControllers.js
@@ -1,9 +1,12 @@
 const note=require('../models/notesModel')
 const asyncHandler=require('express-async-handler')
+const user = require('../models/userModel')
+//getting all notes of user
 const getNotes=asyncHandler(async(req,res)=>{
      const notes=await note.find({user:req.user.id})
      res.json(notes)
 })
+//creating note by given info
 const createNotes=asyncHandler(async(req,res)=>{
       const {title,content,category}=req.body
       if(!title || !category || !content){
@@ -16,4 +19,51 @@ const createNotes=asyncHandler(async(req,res)=>{
           res.status(201).json(createdNote)
       }
 })
-module.exports={getNotes,createNotes}
+//getting single note by id 
+const getNoteById=asyncHandler(async(req,res)=>{
+     const Note=await note.findById(req.params.id)
+     if(Note){
+               res.json(Note)
+     }
+     else{
+               res.status(404).json({message:'Note not found!'})
+     }
+})
+//update note
+const updateNote=asyncHandler(async(req,res)=>{
+          const {title,content,category}=req.body
+          const Note=await note.findById(req.params.id)
+          // checking this note belongs to the logined user or not 
+          if(Note.user.toString()!==req.user.id.toString()){
+                    throw new Error('You can not perform this action!')
+          }
+          // it means the user is verified now we just need to update 
+         if(Note){
+          Note.title=title
+          Note.content=content
+          Note.category=category
+          //saving in db
+          const updatedNote=await Note.save()
+          res.json(updatedNote)
+     }
+     //user is verified but note not found
+     else{
+          throw new Error('Note not found!')
+     }
+})
+//deleting note 
+const deleteNote=asyncHandler(async(req,res)=>{
+     const Note=await note.findById(req.params.id)
+     if(Note.user.toString()!==req.user.id.toString()){
+          throw new Error('You can not delete this note!')
+     }
+     if(Note){
+          await note.deleteOne()
+          res.json({message:'Note deleted!'})
+     }
+     else{
+          res.status(404)
+          throw new Error('Note not found!')
+     }
+})
+module.exports={getNotes,createNotes,getNoteById,updateNote,deleteNote}
--- Backend/routes/notesRoutes.js
+++ Backend/routes/notesRoutes.js
@@ -1,11 +1,13 @@
 const express=require('express')
-const {getNotes} = require('../controllers/notesControllers')
+const {getNotes, getNoteById, updateNote, deleteNote} = require('../controllers/notesControllers')
 const {createNotes} = require('../controllers/notesControllers')
 const { protect } = require('../middlewares/authMiddleware')
 const router=express.Router()
 router.route('/').get(protect,getNotes)
 router.route('/create').post(protect,createNotes)
-// update one single note 
-// router.route('/:id').get().put().delete()  
+// gettig one single note by routing /api/notes/:id->get  
+//updating note by routing /api/notes/:id->put
+// /deleting note by routing /api/notes/:id->del 
+router.route('/:id').get(getNoteById).put(protect,updateNote).delete(protect,deleteNote)
 
 module.exports=router
